69bd1f2882efad0acceda3d797dc5620698c28d7
mtritran <tranmanhtri5104@gmail.com>
2025-11-28 15:14:57 +0700

    initial project

---
+++ src/main/java/com/mtritran/test/github/by/selenium/TestGithubBySeleniumApplication.java
@@ -0,0 +1,13 @@
+package com.mtritran.test.github.by.selenium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGithubBySeleniumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGithubBySeleniumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mtritran/test/github/by/selenium/TestGithubBySeleniumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtritran.test.github.by.selenium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGithubBySeleniumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
